92dae3e39e2eaabac812dff7d54d5c6dec6c415a
rsdk1 <rs.dk.00001@gmail.com>
2025-07-23 13:12:13 +0530

    new changes

--- dummy.js
+++ dummy.js
@@ -3,3 +3,4 @@ var count = 10;
 var sum = 100;
 let total = count * sum;
 console.log(total);
+console.log("hello javascript");
---
+++ firstpro.js
@@ -0,0 +1,6 @@
+var marks = [80,90,92,78,99]
+console.log(marks.length);
+marks.push(100);
+console.log(marks)
+marks.pop(80);
+console.log(marks);
