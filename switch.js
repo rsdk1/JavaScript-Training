656c142e3ae1c567606bd93c5d708c236f113706
rsdk <suma@MacBookAir.lan>
2025-07-28 10:40:47 +0530

    switch practice

---
+++ switch.js
@@ -0,0 +1,28 @@
+var language = "java"
+switch (language)
+{
+    case "java":
+
+    console.log("Write once, run everywhere — but debug everywhere too.");
+    break;
+
+    case "kotlin":
+    console.log("Concise, expressive, safe — Kotlin makes Android a pleasure");
+    break;
+    
+    case "javascript":
+    console.log("Everything is possible in JavaScript — even the impossible.");
+    break;
+
+    case "css":
+    console.log("Where one line can make it stunning or break it all.");
+    break;
+    case "HTML":
+    console.log("The structure behind every beautiful website begins with HTML.");
+    break;
+    
+
+
+default :
+    console.log("nan");
+}
