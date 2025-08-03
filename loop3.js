69eedc4b2896ca5064c6f84a95ee78da96a4a903
rsdk1 <rs.dk.00001@gmail.com>
2025-08-03 09:04:02 +0530

    new changes to be updated

--- loop3.js
+++ loop3.js
@@ -1,6 +1,12 @@
 function price (a,b)
 {
-
+if (a>b)
+{
+    console.log("The first quality apple price is");
+    
+}else{
+    console.log("the second quality apple price is")
+}
  console.log("hello")
 }
 price(130,230);
