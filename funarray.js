01717567177e6fc9f05fbb29eb72312e103f7b8b
rsdk1 <rs.dk.00001@gmail.com>
2025-08-04 16:30:19 +0530

    new

---
+++ funarray.js
@@ -0,0 +1,11 @@
+function printArray(a)
+// functon that returns sum of numbers in arrray
+{
+
+   
+    for (var i=0;i<a.length;i++)
+    {
+        console.log(a[i]);
+    }
+}
+printArray([78,988,98,89,81])
